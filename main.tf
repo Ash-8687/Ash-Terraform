ded7d4292881a240c963008c4397238e94c1a2b4
Ashish Singh <ashishsingh808079@gmail.com>
2026-03-29 22:38:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,56 @@ provider "aws" {
   region = "ap-southeast-2"
 }
 
-resource "aws_s3_bucket" "example" {
-  bucket = "ash-terraform-demo-bucket-2026"
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "ash-test-terraform"
+    values = ["al2023-ami-*-x86_64"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+resource "aws_security_group" "ec2_sg" {
+  name        = "ash-ec2-ssh-sg"
+  description = "Allow SSH inbound traffic"
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "ash-ec2-ssh-sg"
+  }
+}
+
+resource "aws_instance" "web" {
+  ami                    = data.aws_ami.amazon_linux.id
+  instance_type          = "t2.micro"
+  key_name               = "ash-terraform-key"
+  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+
+  tags = {
+    Name = "Ash-Terraform-EC2"
+  }
+}
+
+output "instance_public_ip" {
+  value = aws_instance.web.public_ip
 }
